5a786952a2c6adc26b489e0676d73c0471e405f3
Derek L <derekslarson@gmail.com>
2018-10-11 21:43:06 +0700

    second

--- src/components/Previewer.js
+++ src/components/Previewer.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import Frame from './Frame';
+import converter from '../converter'
 import { connect } from 'react-redux';
 
 const Previewer = props => {
 
   const { input } = props;
-  const newInput = "<p>" + input + "</p>";
-  const newerInput = 
-    newInput.split("")
-            .map(char => char.charCodeAt(0) === 10 ? "<br>" : char)
-            .join("")
-            .replace(/\s\s+/g, '<br>')
-            .replace(/<br><br>/g, '</p><p>')
-            .replace(/<p><\/p>/g, '');
-  console.log(newerInput);
+  const newInput = converter(input);
   return (
     <Frame name="previewer">
-      <div className="conversion" dangerouslySetInnerHTML={{__html: newerInput}}></div>
+      <div className="conversion" dangerouslySetInnerHTML={{__html: newInput}}></div>
     </Frame>
   );
 }
