da98a7c497ad3b11b473fbf44243c1048b305533
Derek L <derekslarson@gmail.com>
2019-01-09 23:09:32 +0700

    changed content

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -6,25 +6,32 @@ import { input } from '../actions';
 class Editor extends Component {
 
   state = {
-    input: `# This is an h1 heading
-## This is an h2 heading
-**This is bold text**
-
-[This is a link](http://www.google.com)
-
-This \`isInlineCode();\`
-
-    function codeBlock() {
-      return 'This is a code block';
-    }
-
-1. This is a list item
-2. this is another list item 
-
-> This is a block quote
->> This one is nested
-
-![This is an image](https://www.gettingstamped.com/wp-content/uploads/2016/02/Things-to-do-in-El-Nido-Philippines-Simizu-Island-El-Nido-Tour-A-1.jpg)`
+    input: `# Derek Larson  
+    **Full Stack Web Developer**  
+    **E-mail** derekslarson@gmail.com  
+    **LinkedIn** [linkedin.com/in/derekslarson](https://linkedin.com/in/derekslarson)  
+    **GitHub** [github.com/cstst](https://github.com/cstst)  
+    
+    ## Summary
+    Full stack web developer proficient in front-end technologies such as React, Redux, Sass, Webpack and Babel, as well as Node.js and MongoDB on the back-end. Experienced in building database-driven web applications with complex functionality. Always striving to learn more and be challenged.
+    
+    Originally from Woodinville, WA, moved to Phnom Penh, Cambodia in 2013 to experience something different. Spent the last five years teaching high school students enrolled in a US school's distance learning program. Recently moved back to the Seattle area to further career as a developer.
+    
+    ## Experience  
+    **11/2018 - 01/2019**   
+    **Dom's Handcrafted Sausages** ([domssausages.com](https://domssausages.com))  
+    **Full Stack Web Developer**  
+       
+    Full e-commerce single-page-application constructed from the ground up. Front-end built using React, Redux, Webpack & Babel. Back-end & CMS built using Node.js, MongoDB & KeystoneJS. Customer account creation and authentication using Passport.js. Order sending using Mailgun. Dual language functionality. Fully responsive using Sass.
+    
+    ## Education
+    **01/2018 - 09/2018**  
+    **freeCodeCamp** ([freecodecamp.org](https://freecodecamp.org))  
+    **Full Stack Web Development**
+      
+    Completed certifications in Responsive Web Design, Algorithms, Data Structures, Front-End Libraries, Data Visualization, APIs, Microservices, Information Security and Quality Assurance, culminating in a final certification in Full Stack Web Development. Built a variety of front and back-end projects such as an anonymous message board, a stock price checker and favorites tracker, a markdown previewer, a choropleth map and a clone of the game Simon.
+    
+    `
   }
   componentDidMount() {
     this.props.input(this.state.input)
